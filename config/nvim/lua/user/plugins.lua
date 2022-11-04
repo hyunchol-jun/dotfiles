284a00cd92ed9dfd76ff78bb0ed8de7e4d8e99c2
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-11-03 21:45:56 -0400

    Change color scheme to tokyonight

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -47,7 +47,7 @@ return packer.startup(function(use)
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
+  use "folke/tokyonight.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
