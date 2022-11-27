3266f7d5d86808a3abef585dc3e78a0ad70aa3ec
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-11-27 18:10:57 -0500

    Add keymap to clear search highlight in nvim

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -37,6 +37,9 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
+-- Clear last search highlight
+keymap("n", "<leader>/", ":noh<cr>", opts)
+
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
