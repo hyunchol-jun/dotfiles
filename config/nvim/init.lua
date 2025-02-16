b6cbb9b03a6c886fb46aaa0c07a6ff6cef83b2fa
Joseph Jun <joseph.hyunchol.jun@gmail.com>
2025-02-16 21:05:35 +0900

    set up vim-dotenv and tweak tmux setting

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -93,6 +93,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Setting autoformat to false at starting, can toggle on later
+vim.g.disable_autoformat = false
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
--- config/nvim/lua/custom/plugins/dadbod.lua
+++ config/nvim/lua/custom/plugins/dadbod.lua
@@ -3,6 +3,7 @@ return {
     'kristijanhusak/vim-dadbod-ui',
     dependencies = {
       { 'tpope/vim-dadbod', lazy = true },
+      { 'tpope/vim-dotenv', lazy = true },
       { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
     },
     cmd = {
