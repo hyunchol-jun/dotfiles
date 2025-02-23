d64c82b51399c4664e8aed6759303fd26c11c936
Joseph Jun <joseph.hyunchol.jun@gmail.com>
2025-02-23 09:16:01 +0900

    add flake files

---
+++ config/nix/flake.nix
@@ -0,0 +1,131 @@
+{ description = "My nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, ... }:
+  let
+    configuration = { pkgs, config, ... }: {
+      nixpkgs.config.allowUnfree = true;
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ 
+	  pkgs.alacritty
+	  pkgs.neovim
+	  pkgs.obsidian
+	  pkgs.tmux
+	  pkgs.htop
+	  pkgs.slack
+	  pkgs.google-chrome
+	  pkgs.git
+	  pkgs.jetbrains.idea-community-bin
+	  pkgs.dbeaver-bin
+	  pkgs.stow
+        ];
+      
+      homebrew = {
+        enable = true;
+	brews = [
+	  "mas"
+	];
+	casks = [
+	  "karabiner-elements"
+	  "raycast"
+	  "opal-composer"
+	  "docker"
+	];
+	onActivation.cleanup = "zap";
+	onActivation.autoUpdate = true;
+	onActivation.upgrade = true;
+      };
+
+      fonts.packages = [
+        pkgs.nerd-fonts.jetbrains-mono
+      ];
+
+      # For applications to appear in spotlight search
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+            '';
+      
+      system.defaults = {
+        dock.autohide = true;
+	dock.persistent-apps = [
+	];
+	loginwindow.GuestEnabled = false;
+	finder.FXPreferredViewStyle = "clmv";
+	NSGlobalDomain.AppleInterfaceStyle = "Dark";
+	NSGlobalDomain.InitialKeyRepeat = 1;
+	NSGlobalDomain.KeyRepeat = 1;
+	NSGlobalDomain._HIHideMenuBar = true;
+	NSGlobalDomain.AppleKeyboardUIMode = 3;
+	NSGlobalDomain.AppleShowAllExtensions = true;
+	NSGlobalDomain.AppleShowAllFiles = true;
+	NSGlobalDomain.NSAutomaticWindowAnimationsEnabled = false;
+	trackpad.Clicking = true;
+	universalaccess.reduceMotion = true;
+      };
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.zsh.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."mini" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+        configuration
+	nix-homebrew.darwinModules.nix-homebrew
+	{
+	  nix-homebrew = {
+	    enable = true;
+	    enableRosetta = true;
+	    user = "hyuncholjun";
+          };
+	}
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."mini".pkgs;
+  };
+}
