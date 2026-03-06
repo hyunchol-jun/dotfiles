051bad8cca075ddb2161692b01d87ddd9cce9074
Joseph Jun <joseph.hyunchol.jun@gmail.com>
2026-03-07 06:27:36 +0900

    chore: add ui to env symlink

--- config/nvim/lua/custom/worktree-tmux-projects.lua
+++ config/nvim/lua/custom/worktree-tmux-projects.lua
@@ -39,6 +39,7 @@ return {
       '.kube/config.user.yaml',
       'packages/implentio-app/packages/toolbelt/public/config/env.js',
       'packages/implentio-app/packages/api-v2/.env',
+      'packages/implentio-app/packages/ui/.env',
       'packages/reconciliation/projects/api/build/resources/main/application-local.yml',
     },
     custom_layout = {
