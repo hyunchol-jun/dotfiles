cadf2bea23d7eec6729ff9db9f7f119a66db35cb
Joseph Jun <joseph.hyunchol.jun@gmail.com>
2026-03-17 10:22:51 +0900

    feat: add markdown-preview.nvim for browser-based markdown preview
    Adds iamcco/markdown-preview.nvim alongside existing render-markdown.nvim.
    Toggle with <leader>mp to open a live GitHub-styled preview in the browser.

--- config/nvim/lua/custom/plugins/markdown.lua
+++ config/nvim/lua/custom/plugins/markdown.lua
@@ -8,4 +8,13 @@ return {
       { '<leader>mt', '<cmd>RenderMarkdown toggle<cr>', desc = 'Toggle render markdown' },
     },
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = 'markdown',
+    build = 'cd app && npm install',
+    keys = {
+      { '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Toggle markdown preview (browser)' },
+    },
+  },
 }
